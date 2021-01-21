81cf850876e192a6decc4115cc8c6d8ea616048e
mousumee2306 <74125571+mousumee2306@users.noreply.github.com>
2021-01-21 17:08:22 +0530

    first commit

---
+++ Resources/js/script.js
@@ -0,0 +1,88 @@
+
+
+$(document).ready(function(){
+  
+/*-------------for sticky navigation--------------*/
+   $('.js--section-features').waypoint(function(direction){
+     if(direction=="down")
+     {
+       $('nav').addClass('sticky');
+     }
+     else{
+       $('nav').removeClass('sticky');
+     
+     }},{
+       offset:'60px'
+     
+   });
+   /*--------scroll on buttons-------------*/
+   $('.js--scroll-to-plans').click(function(){
+     $('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+   });
+   $('.js--scroll-to-start').click(function(){
+    $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+  });
+  /*-------------------------navigation scroll---------------------------------*/
+  $(function(){
+    $('a[href*=#]:not([href=#])').click(function(){
+      if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&& location.hostname==this.hostname){
+        var target=$(this.hash);
+        target=target.length?target:$('[name='+this.hash.slice(1)+']');
+        if(target.length){
+          $('html,body').animate({
+            scrollTop:target.offset().top},1000);
+            return false;
+          }
+      
+      
+    }
+  });
+
+
+});
+/*------------------------Animation on scroll---------------*/
+$('.js--wp-1').waypoint(function(direction){
+  $('.js--wp-1').addClass('animated fadeIn');
+},{
+  offset:'50%'
+
+
+});
+$('.js--wp-2').waypoint(function(direction){
+  $('.js--wp-2').addClass('animated fadeInUp');
+},{
+  offset:'50%'
+});
+
+  $('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animated fadeInUp');
+  },{
+    offset:'50%'
+  });
+
+$('.js--wp-4').waypoint(function(direction){
+  $('.js--wp-4').addClass('animated pulse');
+},{
+  offset:'50%'
+});
+
+/*-------------MOBILE NAVIGATION---------*/
+$('.js--nav-icon').click(function(){
+var nav=$('.js--main-nav');
+var icon=$('.js--nav-icon i')
+nav.slideToggle(200);
+/*if(icon.hasClass('material-icons list')){
+icon.addClass();
+icon.removeClass('material-icons list');
+}
+else{
+  icon.addClass();
+icon.removeClass('material-icons list');
+
+}*/
+});
+});
+
+
+
+
